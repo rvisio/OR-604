aae9f642016f8d8645f528e90e280f8d3c648ab6
= <=robjarvis92@gmail.com>
2016-01-25 19:15:55 -0500

    updating from 1/25/2016

--- HomeworkOne.py
+++ HomeworkOne.py
@@ -4,6 +4,12 @@ Created on Tue Jan 12 23:05:28 2016
 
 @author: Rob
 """
+import os
+import sqlite3
+import csv
+import random
+from math import * 
+dbName = ''
 
 def questionOne(a,b):
     if a%b==0:
@@ -19,7 +25,6 @@ def questionThree(a,b):
     """
 
 def questionFour(n,k):
-    import random
     for i in range(0,k):
         print(random.randint(0,n))
     
@@ -49,24 +54,25 @@ def questionSix():
 #questionSix()
 
 def sqlQuestionOne():
-    import os
-    import sqlite3
-    import csv
+    
     
     # gets path of python script and pulls mcdonalds csv from that directory
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     file = os.path.join(__location__, 'mcDonalds.csv')
     
     # creates db, sets up cursor
-    myConnection = sqlite3.connect('mcD.db')
+    global dbName
+    dbName = 'mcD.db'
+    myConnection = sqlite3.connect(dbName)
     myCursor = myConnection.cursor()
 
     # delete the stores table if it already exists
     deleteSQL = """ DROP TABLE IF EXISTS stores """
     myCursor.execute(deleteSQL)
     myConnection.commit()
-    
+    myCursor.close()
     #create stores table
+    myCursor = myConnection.cursor()
     createTableSQL = """ CREATE TABLE IF NOT EXISTS stores 
                          (name text,
                          address text,
@@ -85,7 +91,7 @@ def sqlQuestionOne():
     myReader = csv.reader(myFile)
     
     tempList = []
-    
+    iterator = 0
     # insert values into stores table
     for row in myReader:
         tempList.append(tuple(row))
@@ -93,7 +99,9 @@ def sqlQuestionOne():
             myCursor.executemany('INSERT INTO stores VALUES(?,?,?,?,?,?,?,?);',tempList)
             tempList =[]
             myConnection.commit()
-        
+    
+    myCursor.executemany('INSERT INTO stores VALUES(?,?,?,?,?,?,?,?);',tempList)
+    myConnection.commit()
     # clean up
     myCursor.close()
     myConnection.close()
@@ -103,6 +111,101 @@ def sqlQuestionOne():
 
 sqlQuestionOne()
 
+def sqlQuestionTwo():
+    dist = 0.0
+    withinDistList = []
+    lat = 0.0
+    lon= 0.0
+    lat2= 0.0
+    lon2= 0.0 
+     # sets database to db defined in sql question one, using python file directory
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    myDB = __location__ + "\\" + dbName
+    myConnection = sqlite3.connect(myDB)
+    myCursor = myConnection.cursor()
+    
+    # iterate through db to find min/max lat/lon for all mcdonalds in state of NY
+    nyLatSQLString = """ SELECT lat,lon,storeNumber FROM stores WHERE state = 'NY' """
+    myCursor.execute(nyLatSQLString)
+    
+    coordList = []
+    # store lat/lon information from all mcdonalds in NY
+    while True:
+        rows = myCursor.fetchmany(5000)
+        if not rows:
+            break
+        else:
+            for store in rows:
+                coordList.append([store[0],store[1],store[2]]) # lat/lon and store number
+  
+    myCursor.close()
+    myCursor = myConnection.cursor()
+    mcdSQLString = """ SELECT lat,lon, storeNumber FROM stores WHERE state != 'NY' """
+    myCursor.execute(mcdSQLString)
+    
+    iterator = 0
+    tempCoord = list()
+    while True: 
+        rows = myCursor.fetchmany(5000)
+        if not rows:
+            break
+        else:
+            for x in range(len(coordList)):
+                tempCoord = coordList[x]
+                lat = float(tempCoord[0])
+                lon = float(tempCoord[1])
+               # print str(tempCoord[0]) + " " + str(tempCoord[1]) + " " +  str(tempCoord[2])
+                for y in rows:
+                    lat2 = float(y[0])
+                    lon2 = float(y[1]) 
+                    #print str(y[0]) + str(y[1]) + " " + str(y[2])
+                    dist = haversine(lon,lat,lon2,lat2)
+                    print dist
+                  
+                   
 
+                
+                
+       
+    # use haversine formula to calculate lat/lon within 100 miels based on min/max lat/lon
+    
+    # create new table using values of mcdonalds in NY and mc donalds within 100 miles
+    
+    # cleanup
+    
+# haversine function calculates distance between two coordinate points
+def haversine(lon1,lat1, lon2, lat2):
+    
+    lon1,lat1,lon2,lat2= map(radians, [lon1,lat1,lon2,lat2])
+    
+    dlon = lon2-lon1
+    dlat = lat2-lat1
+    
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 3956 
+    return c *r
+    
+
+sqlQuestionTwo()
+
+
+def sqlQuestionThree():
+    pass
+    #implemented in sqlQuestionOne
+#    for row in myReader:
+#        tempList.append(tuple(row))
+#        if len(tempList) % 5000 == 0:
+#            myCursor.executemany('INSERT INTO stores VALUES(?,?,?,?,?,?,?,?);',tempList)
+#            tempList =[]
+#            myConnection.commit()
+#    
+#    myCursor.executemany('INSERT INTO stores VALUES(?,?,?,?,?,?,?,?);',tempList)
+#    myConnection.commit()
+#    # clean up
+#    myCursor.close()
+#    myConnection.close()
+#    myFile.close()
+#    myReader = None
 
     
