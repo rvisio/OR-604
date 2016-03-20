bedef81780533978fad4175d94af04eb482eecdc
= <=robjarvis92@gmail.com>
2016-03-20 16:58:10 -0400

    HW updates...

--- pizza/Homework6.py
+++ pizza/Homework6.py
@@ -66,14 +66,6 @@ for curStore in dcStoreList:
             for x in rows:
                 storeLoc[curStore] = (float(x[6]),float(x[7]))
 
-# create capacity dictionary -- based off of capacity of each truck
-# Produce = key, value = dough
-capacity = {44:0,41:1,39:2,37:3,34:4,
-            32:5,30:6,28:7,26:8,24:9,
-            22:10,19:11,17:12,15:13,
-            13:14,10:15,8:16,6:17,
-            4:18,2:19,0:20}
-
 # Dictionary of distance from dc 5 to store
 # also add in bearing
 bearing = 0.0
@@ -83,11 +75,12 @@ doughDemand = 0.0
 storeBearing = []
 
 demandSQL = """SELECT * FROM avgStoreDemand WHERE storeNumber == (?)"""
+storeCoordDict = {}
 for i in storeLoc:
     #TODO
     # Update haversine distance to google maps api
     hDistance = haversine(dcLat, dcLon, storeLoc[i][0],storeLoc[i][1])
-
+    storeCoordDict[i] = storeLoc[i][0],storeLoc[i][1]
     #Calculate  bearing
     y = sin(storeLoc[i][1]-dcLon)* cos(storeLoc[i][0])
     x = cos(dcLat)*sin(storeLoc[i][0]) - sin(dcLat)*cos(storeLoc[i][0])*cos(storeLoc[i][1]-dcLon)
@@ -107,25 +100,84 @@ for i in storeLoc:
 
     #TODO
     # Determine number of product palletes
+    # Lets just assume product pallets is 1/2 of dough pallets
 
-    doughPallets = ceil(doughDemand/180.0)
-    tempList = [int(i), bearing, hDistance, doughPallets]
+    doughPallets = ceil(doughDemand/180)
+    productPallets = ceil(doughPallets * 2)
+    tempList = [int(i), bearing, hDistance, int(productPallets),int(doughPallets)]
     storeBearing.append(tempList)
 
 # Sort list based on bearing
 storeBearing = sorted(storeBearing, key=itemgetter(1))
 
 
-for i in storeBearing:
-    print i
+
 # Dough needed will be determined from avgStoreDemand
 # 85% of supply normal dough 15% of za is crispy thin crust
 # 1.5 cup sauce per pizza
 # 2.25 cups of cheese
 
-# One dough pallete = 180 doughs
-# product pallete can store 12 cheese units, 10 sauce units, or 6 thin crust per tray
-# lets assume 8 trays per product pallete
+
+# create capacity dictionary -- based off of capacity of each truck
+# Produce = key, value = dough
+capacity = {44:0,43:0,42:0,41:1,40:1,39:2,38:2,37:3,36:3,35:3,34:4,
+            33:4,32:5,31:5,30:6,29:6,28:7,27:7,26:8,25:8,24:9,
+            23:9,22:10,21:10,20:10,19:11,18:11,17:12,16:12,15:13,
+            14:13,13:14,12:14,11:14,10:15,9:15,8:16,7:16,6:17,
+            5:17,4:18,3:18,2:19,1:19,0:20}
+
+# Begin Routing Heuristic
+
+# ALL TIMES IN MINUTES
+# ALL DISTANCE IN MILES
+# ALL ZA IN DOMINOS
+numTrucks = 1
+truckLocation = "DistCenter"
+truckTime = 0
+#product, dough
+truckCapacity = [0,0]
+truckRoute = {}
+serviceTime = 45
+
+for store in storeBearing:
+    AddStore = False
+    newProductTotal = truckCapacity[0] + store[3]    # total pallete of current truck capacity and store required
+    # Check truck capacity against current capacity
+    try:
+        if capacity[newProductTotal] > truckCapacity[1]:
+            #TODO
+            # Update haversine to GOOGLE API
+            # Currently assuming distance in miles divided by 55 mph (max speed for truckers)
+            curStoreStr = str(store[0])
+            print curStoreStr
+            curStoreLat = storeCoordDict[curStoreStr][0]
+            curStoreLon = storeCoordDict[curStoreStr][1]
+
+            # Time from current store back to distribution center
+            timeBackToDC = (haversine(dcLat,dcLon,curStoreLat,curStoreLon))/55
+
+            if timeBackToDC+truckTime+serviceTime < 14*60:    # If we can get back to store and not be over 14 hours
+                 # add store to route this truck is taking
+                truckRoute.append(store)
+                 # add values to truckCapacity
+                truckCapacity[0] += store[3]
+                truckCapacity[1] += store[4]
+                # add time to truckTime
+                truckTime += serviceTime+(store[2]/55)+serviceTime
+                truckLocation = store[0]
+                AddStore = True
+    except:
+        if AddStore != True:  # truck needs to head back to depot, this route is complete
+            print 'uh oh'
+            #TODO
+            # Record truck route
+            # Update truck route to dc -> cur store
+            # TruckLocation = next store
+            # Truck capacity = next store product, dough
+            # update trucktime
+            numTrucks += 1
+
+
 
 
 
