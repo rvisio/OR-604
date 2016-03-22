d574819106c6286c7b80c32a39fcd643efc5e5c7
= <=robjarvis92@gmail.com>
2016-03-21 20:56:29 -0400

    HW updates...

--- pizza/Homework6.py
+++ pizza/Homework6.py
@@ -2,26 +2,26 @@ import csv, sqlite3
 from math import *
 from operator import itemgetter
 
-#Set up sqlite db
+# Set up sqlite db
 dbName = 'hw_four.db'
 myConnection = sqlite3.connect(dbName)
 myCursor = myConnection.cursor()
 
+
 # Haversine function to determine travel time
-#TODO
+# TODO
 # Should really be using google maps api -- will switch after testing with haversine
 # Switch to google maps and store result in database so we don't need to call over and over
-def haversine(lat1,lon1,lat2,lon2):
+def haversine(lat1, lon1, lat2, lon2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    lon1,lat1,lon2,lat2= map(radians, [lon1,lat1,lon2,lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
 
-    dlon = lon2-lon1
-    dlat = lat2-lat1
-
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
     r = 3956
-    return c *r
+    return c * r
 
 
 # Using answer from hw4 select all stores that have dc 5 has distribution center
@@ -29,23 +29,22 @@ answerSQL = "SELECT * FROM Answer"
 myCursor.execute(answerSQL)
 dcStoreList = []
 while True:
-        rows = myCursor.fetchall()
-        if not rows:
-            break
-        else:
-            for store in rows:
-                if store[0] == '5':
-                    dcStoreList.append(store[1])  # append store number
+    rows = myCursor.fetchall()
+    if not rows:
+        break
+    else:
+        for store in rows:
+            if store[0] == '5':
+                dcStoreList.append(store[1])  # append store number
 
-
-#Only using distribution center 5
+# Only using distribution center 5
 myFile = open(r'dist.csv', 'rt')
 myReader = csv.reader(myFile)
 # get dc 5 and store in dc var
 # dc type will be list
 dc = ""
 for row in myReader:
-    if row[0] =='5':
+    if row[0] == '5':
         dc = row
         break
 dcLat = float(dc[2])
@@ -56,7 +55,7 @@ storeLoc = {}
 tempVal = ()
 storeSQL = """SELECT * FROM stores WHERE storeNumber == (?)"""
 for curStore in dcStoreList:
-    val =  str(curStore)
+    val = str(curStore)
     myCursor.execute(storeSQL, (val,))
     while True:
         rows = myCursor.fetchall()
@@ -64,7 +63,7 @@ for curStore in dcStoreList:
             break
         else:
             for x in rows:
-                storeLoc[curStore] = (float(x[6]),float(x[7]))
+                storeLoc[curStore] = (float(x[6]), float(x[7]))
 
 # Dictionary of distance from dc 5 to store
 # also add in bearing
@@ -77,14 +76,14 @@ storeBearing = []
 demandSQL = """SELECT * FROM avgStoreDemand WHERE storeNumber == (?)"""
 storeCoordDict = {}
 for i in storeLoc:
-    #TODO
+    # TODO
     # Update haversine distance to google maps api
-    hDistance = haversine(dcLat, dcLon, storeLoc[i][0],storeLoc[i][1])
-    storeCoordDict[i] = storeLoc[i][0],storeLoc[i][1]
-    #Calculate  bearing
-    y = sin(storeLoc[i][1]-dcLon)* cos(storeLoc[i][0])
-    x = cos(dcLat)*sin(storeLoc[i][0]) - sin(dcLat)*cos(storeLoc[i][0])*cos(storeLoc[i][1]-dcLon)
-    bearing = atan2(y,x)
+    hDistance = haversine(dcLat, dcLon, storeLoc[i][0], storeLoc[i][1])
+    storeCoordDict[i] = storeLoc[i][0], storeLoc[i][1]
+    # Calculate  bearing
+    y = sin(storeLoc[i][1] - dcLon) * cos(storeLoc[i][0])
+    x = cos(dcLat) * sin(storeLoc[i][0]) - sin(dcLat) * cos(storeLoc[i][0]) * cos(storeLoc[i][1] - dcLon)
+    bearing = atan2(y, x)
 
     curStore = str(i)
     myCursor.execute(demandSQL, (curStore,))
@@ -96,21 +95,19 @@ for i in storeLoc:
             break
         else:
             for x in rows:
-                doughDemand =  float(x[2])
+                doughDemand = float(x[2])
 
-    #TODO
+    # TODO
     # Determine number of product palletes
     # Lets just assume product pallets is 1/2 of dough pallets
 
-    doughPallets = ceil(doughDemand/180)
+    doughPallets = ceil(doughDemand / 180)
     productPallets = ceil(doughPallets * 2)
-    tempList = [int(i), bearing, hDistance, int(productPallets),int(doughPallets)]
+    tempList = [int(i), bearing, hDistance, int(productPallets), int(doughPallets)]
     storeBearing.append(tempList)
 
 # Sort list based on bearing
 storeBearing = sorted(storeBearing, key=itemgetter(1))
-
-
 
 # Dough needed will be determined from avgStoreDemand
 # 85% of supply normal dough 15% of za is crispy thin crust
@@ -120,11 +117,12 @@ storeBearing = sorted(storeBearing, key=itemgetter(1))
 
 # create capacity dictionary -- based off of capacity of each truck
 # Produce = key, value = dough
-capacity = {44:0,43:0,42:0,41:1,40:1,39:2,38:2,37:3,36:3,35:3,34:4,
-            33:4,32:5,31:5,30:6,29:6,28:7,27:7,26:8,25:8,24:9,
-            23:9,22:10,21:10,20:10,19:11,18:11,17:12,16:12,15:13,
-            14:13,13:14,12:14,11:14,10:15,9:15,8:16,7:16,6:17,
-            5:17,4:18,3:18,2:19,1:19,0:20}
+capacity = {50: 0, 49: 0, 48: 0, 47: 0, 46: 0, 45: 0, 44: 0, 43: 0, 42: 0, 41: 1, 40: 1, 39: 2, 38: 2, 37: 3, 36: 3,
+            35: 3, 34: 4,
+            33: 4, 32: 5, 31: 5, 30: 6, 29: 6, 28: 7, 27: 7, 26: 8, 25: 8, 24: 9,
+            23: 9, 22: 10, 21: 10, 20: 10, 19: 11, 18: 11, 17: 12, 16: 12, 15: 13,
+            14: 13, 13: 14, 12: 14, 11: 14, 10: 15, 9: 15, 8: 16, 7: 16, 6: 17,
+            5: 17, 4: 18, 3: 18, 2: 19, 1: 19, 0: 20}
 
 # Begin Routing Heuristic
 
@@ -134,60 +132,66 @@ capacity = {44:0,43:0,42:0,41:1,40:1,39:2,38:2,37:3,36:3,35:3,34:4,
 numTrucks = 1
 truckLocation = "DistCenter"
 truckTime = 0
-#product, dough
-truckCapacity = [0,0]
-truckRoute = {}
+# product, dough
+truckCapacity = [0, 0]
+truckRoute = []
 serviceTime = 45
+finalRoute = []
 
 for store in storeBearing:
     AddStore = False
-    newProductTotal = truckCapacity[0] + store[3]    # total pallete of current truck capacity and store required
+    newProductTotal = truckCapacity[0] + store[3]  # total pallete of current truck capacity and store required
     # Check truck capacity against current capacity
-    try:
-        if capacity[newProductTotal] > truckCapacity[1]:
-            #TODO
-            # Update haversine to GOOGLE API
-            # Currently assuming distance in miles divided by 55 mph (max speed for truckers)
-            curStoreStr = str(store[0])
-            print curStoreStr
-            curStoreLat = storeCoordDict[curStoreStr][0]
-            curStoreLon = storeCoordDict[curStoreStr][1]
 
-            # Time from current store back to distribution center
-            timeBackToDC = (haversine(dcLat,dcLon,curStoreLat,curStoreLon))/55
+    # Get current store coordinates
+    curStoreStr = str(store[0])
+    curStoreLat = storeCoordDict[curStoreStr][0]
+    curStoreLon = storeCoordDict[curStoreStr][1]
 
-            if timeBackToDC+truckTime+serviceTime < 14*60:    # If we can get back to store and not be over 14 hours
-                 # add store to route this truck is taking
-                truckRoute.append(store)
-                 # add values to truckCapacity
-                truckCapacity[0] += store[3]
-                truckCapacity[1] += store[4]
-                # add time to truckTime
-                truckTime += serviceTime+(store[2]/55)+serviceTime
-                truckLocation = store[0]
-                AddStore = True
-    except:
-        if AddStore != True:  # truck needs to head back to depot, this route is complete
-            print 'uh oh'
-            #TODO
-            # Record truck route
-            # Update truck route to dc -> cur store
-            # TruckLocation = next store
-            # Truck capacity = next store product, dough
-            # update trucktime
-            numTrucks += 1
+    print "Truck Capacity currently equal to "
+    print truckCapacity
 
+    if capacity[newProductTotal] > truckCapacity[1]:
+        # TODO
+        # Update haversine to GOOGLE API
+        # Currently assuming distance in miles divided by 55 mph (max speed for truckers)
+        # Time from current store back to distribution center
+        timeBackToDC = (haversine(dcLat, dcLon, curStoreLat, curStoreLon)) / 55.0
 
+        if timeBackToDC + truckTime + serviceTime < 14 * 60:  # If we can get back to store and not be over 14 hours
+            # add store to route this truck is taking
+            truckRoute.append(store[0])
+            print("TruckRoute Currently equal to ")
+            print truckRoute
+            # add values to truckCapacity
+            truckCapacity[0] += store[3]
+            truckCapacity[1] += store[4]
+            print "Truck Capacity currently at"
+            print truckCapacity
+            # add time to truckTime
+            truckTime += serviceTime + (store[2] / 55) + serviceTime
+            print "truckTime currently at"
+            print truckTime
+            truckLocation = store[0]
+            AddStore = True
 
+    print "WE HERE"
+    if AddStore != True:  # truck needs to head back to depot, this route is complete
+        # TODO
+        # Record truck route
+        print truckRoute
+        finalRoute.append(tuple(truckRoute))
+        # Update truck route to dc -> cur store
+        truckRoute = ["DistCenter"]
+        truckRoute.append(store[0])
+        # TruckLocation = next store
+        truckLocation = store[0]
+        # Truck capacity = next store product, dough
+        truckCapacity[0] = store[3]
+        truckCapacity[1] = store[4]
+        # update trucktime to distance to store
+        truckTime = (haversine(dcLat, dcLon, curStoreLat, curStoreLon) / 55.0) + serviceTime
 
+        numTrucks += 1
 
-
-
-
-
-
-
-
-
-
-
+print finalRoute
